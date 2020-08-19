52ed1403b9f3596caffdc7cc257be50012672cbc
LusineE <64039235+LusineE@users.noreply.github.com>
2020-08-19 12:56:11 +0400

    correction

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
- const fetchAvatar = async () => {
+const container=document.getElementById('div1')
+const image=document.getElementsByTagName('img')[0]
+const p1=document.getElementsByTagName('p')[1]
+const fetchAvatar = async () => {
    const login=document.getElementById('userlogin').value
    const orgUrl = 'https://api.github.com/users/'+login
    try {
      const fetchResult = await fetch(orgUrl)
      const orgData = await fetchResult.json()
      const fetchAvatarResult = await fetch(orgData.avatar_url)
+     const fetchLoginResult= orgData.login
      let imgAvatar=fetchAvatarResult.url
-     const container=document.getElementById('div1')
-    //  container.removeChild
-     const imgElement = document.createElement("img")
-     imgElement.setAttribute("src", imgAvatar)
-     imgElement.style.width="200px"
-     container.appendChild(imgElement)
+     image.setAttribute("src", imgAvatar)
+     p1.innerHTML = fetchLoginResult
    }
    catch(err) {
-     const h1Element = document.createElement('h1');
-     h1Element.innerHTML = "Օգտատերը չի գտնվել"
-     document.body.appendChild(h1Element)
+     p1.innerHTML = "Օգտատերը չի գտնվել"
+     image.setAttribute("src", "")
    }
- }
+}
